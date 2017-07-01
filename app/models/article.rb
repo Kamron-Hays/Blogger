a1a096fd23f88593a5fe732b8feb7d0d9e73fa76
Kamron Hays <kamron.hays@live.com>
2017-07-01 08:26:22 -0400

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,11 +2,13 @@ class Article < ActiveRecord::Base
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
-  
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
   def tag_list
     tags.join(", ")
   end
-  
+
   def tag_list=(tags_string)
     # The specified string will look something like "tag1, tag2, tag3".
     # Split the specified string into individual tags, remove whitespace, make all tag letters
